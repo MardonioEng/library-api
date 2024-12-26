eca49c6e1a9488cf6ab7902c0f3bc511cf48c99f
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2024-12-26 16:35:26 -0300

    Project created

---
+++ src/test/java/com/mardoniodev/libraryapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardoniodev.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
